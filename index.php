7ccdaba072816e70bcf08bf55f2410ef712fa6ff
almirah <string2014@yahoo.com>
2015-04-25 13:11:22 -0700

    init files

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>My Blog Site</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="fonts/glyphicons-halflings-regular.svg">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container ">
+    <div class="jumbotron row">
+        <img src="img/iam.jpg" alt="my pic is here" class="img-responsive col-md-5">
+        <div  class="col-md-5">
+            <ul class="list-group">
+              <i class="text-info"> Name</i><i class="glyphicon glyphicon-user text-info pull-right" aria-hidden="true"> </i> <li class="list-group-item text-info">Abbas Ali</li>
+                <i class="text-info"> Age </i> <i class="glyphicon glyphicon-hourglass text-info pull-right" aria-hidden="true"> </i> <li class="list-group-item text-info">28</li>
+                <i class="text-info"> Live </i> <i class="glyphicon glyphicon-globe text-success pull-right" aria-hidden="true"> </i> <li class="list-group-item text-info">Pakistan Karachi</li>
+                <i class="text-info"> Qualified </i> <i class="glyphicon glyphicon-education pull-right" aria-hidden="true"> </i> <li class="list-group-item text-info">Non graduate In Computer science</li>
+                <i class="text-info"> Using Technology </i> <i class="glyphicon glyphicon-apple text-danger pull-right" aria-hidden="true"> </i> <li class="list-group-item text-info">Html5, Css3, NodeJs, Phonegap ionic, angularJs </li>
+                <i class="text-info"> Certificate </i> <i class="glyphicon glyphicon-pencil text-info pull-right" aria-hidden="true"> </i> <li class="list-group-item text-info">2 year webApp Designing & Developing From Ssuet khi</li>
+                <i class="text-info"> Phone </i> <i class="glyphicon glyphicon-phone text-info pull-right" aria-hidden="true"> </i> <li class="list-group-item text-info">92 03212035181</li>
+            </ul>
+            <!--<h3> Hello peoples ..<br />-->
+        <!--My Name Abbas Ali <br />-->
+            <!--Iam from  Pakistan Karachi<br />-->
+            <!--Qualified Non graduate In computer science -->
+        <!--</h3>-->
+            </div>
+        <a href="https://www.facebook.com/str.ing.967" <i class="text-info col-md-4">contect face book</i> <i class="glyphicon glyphicon-user text-info pull-right" aria-hidden="true"> </i></a>
+        <!--<a href="#" class="thumbnail">-->
+
+        <!--</a>-->
+    </div>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="img/ionic.jpg" alt="...">
+                <div class="carousel-caption">
+                    <h3 style="color: #000000;">Ionic Html Frame Work Gives your designing drams to true</h3>
+                    <p style="color: #000000;">One of the Best Framework for JavaScript Very easy to use
+                        <i clsas="text-warning">Awesome </i>
+                    </p>
+                </div>
+            </div>
+            <div class="item">
+                <img src="img/ionic1.jpg" alt="...">
+                <div class="carousel-caption">
+                    <h3 style="color: #000000;">ionic works like Magic some code  & big view </h3>
+                    <p style="color: #000000;">ionic Html Frame work one of the best for Mobile Apps
+                        <i clsas="text-success">Awesome </i>
+                    </p>                </div>
+            </div>
+            <div class="item">
+                <img src="img/angular.jpg" alt="...">
+                <div class="carousel-caption">
+                    <h3 style="color: #000000;">Angular The power of JavaScript Frame work</h3>
+                    <p style="color: #000000;">One of the Best Framework for JavaScript Very easy to use
+                        <i clsas="text-warning">Awesome </i>
+                    </p>
+                </div>
+            </div>
+            <!--<div class="item">-->
+                <!--<img src="img/iam.jpg" alt="...">-->
+                <!--<div class="carousel-caption">-->
+                    <!--...-->
+                <!--</div>-->
+            <!--</div>-->
+            ...
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div class="row">
+<div class="col-md-8 well">
+    <div class="list-group">
+        <div  class="list-group-item active">
+            <h4 class="list-group-item-heading">Ionic Html frame Work</h4>
+            <p class="list-group-item-text">This is time to biggest change every where peoples going to smallest
+            mobile devices any one have one mobile device for
+                <i class="text-default"> personal use..</i>
+                <i class="glyphicon glyphicon-ok"style="color: cyan" data-toggle="modal" data-target="#myModal">Next</i></i> </p>
+            <img src="img/ionic.jpg" class="img-responsive" alt="Responsive image">
+            <!--<div class="btn-group">-->
+                <!--<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-expanded="false">-->
+                    <!--Action <span class="caret"></span>-->
+                <!--</button>-->
+                <!--<ul class="dropdown-menu" role="menu">-->
+                    <!--<li><a href="#">Ionic nav bars</a></li>-->
+                    <!--<li><a href="#">Ionic bottom sheet</a></li>-->
+                    <!--<li><a href="#">ionic modal</a></li>-->
+                    <!--<li class="divider"></li>-->
+                    <!--<li><a href="#">Leran more..</a></li>-->
+                <!--</ul>-->
+            <!--</div>-->
+        </div>
+
+    </div>
+
+</div>
+        <div class="col-lg-4">
+            <img src="img/ionic1.jpg" class="img-responsive"/>
+            <img src="img/angular.jpg" class="img-responsive"/>
+        </div>
+
+    </div>
+        <!-- Button trigger modal -->
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title text-danger" id="myModalLabel">who want my cv</h4>
+                    </div>
+                    <div class="modal-body"><div class="jumbotron">
+                        <img src="img/modal.jpg" clas="img-responsive"/>
+
+                    </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--<div class="col-md-8 well">-->
+        <!--<div class="list-group">-->
+            <!--<div  class="list-group-item active">-->
+                <!--<h4 class="list-group-item-heading">Ionic Html frame Work</h4>-->
+                <!--<p class="list-group-item-text">This is time to biggest change every where peoples going to smallest-->
+                    <!--mobile devices any one have one mobile device for<i class="text-info"> personal use</i> </p>-->
+                <!--<div class="btn-group">-->
+                    <!--<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">-->
+                        <!--Action <span class="caret"></span>-->
+                    <!--</button>-->
+                    <!--<ul class="dropdown-menu" role="menu">-->
+                        <!--<li><a href="#">Action</a></li>-->
+                        <!--<li><a href="#">Another action</a></li>-->
+                        <!--<li><a href="#">Something else here</a></li>-->
+                        <!--<li class="divider"></li>-->
+                        <!--<li><a href="#">Separated link</a></li>-->
+                    <!--</ul>-->
+                <!--</div>-->
+            <!--</div>>-->
+
+        <!--</div>-->
+
+    <!--</div>-->
+
+
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
